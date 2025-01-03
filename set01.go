55f8830d2900e617d398115e2747a2f49e59c69b
Matthieu <176236412+s0foo@users.noreply.github.com>
2025-01-03 09:08:13 +0100

    Challenge 2

--- set01.go
+++ set01.go
@@ -14,3 +14,14 @@ func HexToBase64(hs string) (string, error) {
 	log.Printf("%s", bs)
 	return base64.StdEncoding.EncodeToString(bs), err
 }
+
+func Xor(a, b []byte) []byte {
+        if len(a) != len(b) {
+                panic("Xor: mismatched lengths")
+        }
+        c := make([]byte, len(a))
+        for i := range len(a) {
+                c[i] = a[i] ^ b[i]
+        }
+        return c
+}
