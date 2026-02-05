cb5a8d275cf7b3effc231fab572df7c97f9bd8d4
s0foo <mtth.grd@laposte.net>
2026-02-05 20:59:33 +0100

    Solve problem 4

--- set1.go
+++ set1.go
@@ -1,79 +1,99 @@
 package cryptopals
 
 import (
-        "encoding/base64"
-        "encoding/hex"
+	"encoding/base64"
+	"encoding/hex"
 )
 
 func hex2Base64(hexString string) (string, error) {
-        res, err := hex.DecodeString(hexString)
-        if err != nil {
-                return "", err
-        }
-        return base64.StdEncoding.EncodeToString(res), nil
+	res, err := hex.DecodeString(hexString)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(res), nil
 }
 
 func xor(a, b []byte) []byte {
-        l := len(a)
-        if l != len(b) {
-                panic("[xor] Buffers must have the same length.")
-        }
-        c := make([]byte, l)
-        for i := 0; i < l; i++ {
-                c[i] = a[i] ^ b[i]
-        }
-        return c
+	l := len(a)
+	if l != len(b) {
+		panic("[xor] Buffers must have the same length.")
+	}
+	c := make([]byte, l)
+	for i := 0; i < l; i++ {
+		c[i] = a[i] ^ b[i]
+	}
+	return c
 }
 
 func letterFrequency(text string) map[rune]float64 {
-        freq := make(map[rune]float64)
-        total := 0.0
+	freq := make(map[rune]float64)
+	total := 0.0
 
-        for _, char := range text {
-                freq[char]++
-                total++
-        }
+	for _, char := range text {
+		freq[char]++
+		total++
+	}
 
-        for char := range freq {
-                freq[char] = (freq[char] / total)
-        }
+	for char := range freq {
+		freq[char] = (freq[char] / total)
+	}
 
-        return freq
+	return freq
 }
 
 func englishLetterFrequencies() map[rune]float64 {
-        return map[rune]float64{
-                'a': 0.06517, 'b': 0.01242, 'c': 0.02173, 'd': 0.03492,
-                'e': 0.10414, 'f': 0.01978, 'g': 0.01586, 'h': 0.04928,
-                'i': 0.05580, 'j': 0.00090, 'k': 0.00507, 'l': 0.03314,
-                'm': 0.02021, 'n': 0.05645, 'o': 0.05963, 'p': 0.01376,
-                'q': 0.00086, 'r': 0.04971, 's': 0.05157, 't': 0.07293,
-                'u': 0.02251, 'v': 0.00829, 'w': 0.01712, 'x': 0.00136,
-                'y': 0.01459, 'z': 0.00074, ' ': 0.18288,
-        }
+	return map[rune]float64{
+		'a': 0.06517, 'b': 0.01242, 'c': 0.02173, 'd': 0.03492,
+		'e': 0.10414, 'f': 0.01978, 'g': 0.01586, 'h': 0.04928,
+		'i': 0.05580, 'j': 0.00090, 'k': 0.00507, 'l': 0.03314,
+		'm': 0.02021, 'n': 0.05645, 'o': 0.05963, 'p': 0.01376,
+		'q': 0.00086, 'r': 0.04971, 's': 0.05157, 't': 0.07293,
+		'u': 0.02251, 'v': 0.00829, 'w': 0.01712, 'x': 0.00136,
+		'y': 0.01459, 'z': 0.00074, ' ': 0.18288,
+	}
 }
 
 func scoreText(text string) float64 {
-        observedFreq := letterFrequency(text)
-        expectedFreq := englishLetterFrequencies()
+	observedFreq := letterFrequency(text)
+	expectedFreq := englishLetterFrequencies()
 
-        score := 0.0
+	score := 0.0
 
-        for char, expected := range expectedFreq {
-                observed, exists := observedFreq[char]
-                if !exists {
-                        observed = 0.0
-                }
-                score += (1 - (observed-expected)*(observed-expected))
-        }
+	for char, expected := range expectedFreq {
+		observed, exists := observedFreq[char]
+		if !exists {
+			observed = 0.0
+		}
+		score += (1 - (observed-expected)*(observed-expected))
+	}
 
-        return score
+	return score
 }
 
 func singleXor(src []byte, key byte) []byte {
-        output := make([]byte, len(src))
-        for i, b := range src {
-                output[i] = b ^ key
-        }
-        return output
+	output := make([]byte, len(src))
+	for i, b := range src {
+		output[i] = b ^ key
+	}
+	return output
+}
+
+func singleCharacterXorFinder(masked []byte) ([]byte, byte) {
+	var key byte
+	var maxScore float64
+	var res []byte
+	var score float64
+	var unmasked []byte
+
+	for k := 0; k < 256; k++ {
+		unmasked = singleXor(masked, byte(k))
+		score = scoreText(string(unmasked))
+		if score > maxScore {
+			maxScore = score
+			key = byte(k)
+			res = unmasked
+		}
+	}
+
+	return res, key
 }
--- set1_test.go
+++ set1_test.go
@@ -1,45 +1,61 @@
 package cryptopals
 
 import (
-        "encoding/hex"
-        "testing"
+	"bufio"
+	"encoding/hex"
+	"os"
+	"testing"
 )
 
 func TestChallenge1(t *testing.T) {
-        res, err := hex2Base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
-        if err != nil {
-                t.Fatal(err)
-        }
-        if res != "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" {
-                t.Error("Wrong output:", res)
-        }
+	res, err := hex2Base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res != "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" {
+		t.Error("Wrong output:", res)
+	}
 }
 
 func TestChallenge2(t *testing.T) {
-        b1, _ := hex.DecodeString("1c0111001f010100061a024b53535009181c")
-        b2, _ := hex.DecodeString("686974207468652062756c6c277320657965")
-        res := hex.EncodeToString(xor(b1, b2))
-        if res != "746865206b696420646f6e277420706c6179" {
-                t.Error("Wrong output:", res)
-        }
+	b1, _ := hex.DecodeString("1c0111001f010100061a024b53535009181c")
+	b2, _ := hex.DecodeString("686974207468652062756c6c277320657965")
+	res := hex.EncodeToString(xor(b1, b2))
+	if res != "746865206b696420646f6e277420706c6179" {
+		t.Error("Wrong output:", res)
+	}
 }
 
 func TestChallenge3(t *testing.T) {
-        m, _ := hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
-        var key byte
-        var maxScore float64
-        var res string
-        var score float64
-        var unmasked []byte
-        for k := 0; k < 256; k++ {
-                unmasked = singleXor(m, byte(k))
-                score = scoreText(string(unmasked))
-                if score > maxScore {
-                        maxScore = score
-                        key = byte(k)
-                        res = string(unmasked)
-                }
-        }
-        t.Logf("Key: %c", key)
-        t.Logf("Message: %s", res)
+	m, _ := hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	res, key := singleCharacterXorFinder(m)
+	t.Logf("Key: %c", key)
+	t.Logf("Message: %s", string(res))
+}
+
+func TestChallenge4(t *testing.T) {
+	file, err := os.Open("data/4.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	defer file.Close()
+
+	score := 0.0
+	maxScore := 0.0
+
+	var masked []byte
+	var unmasked []byte
+	var res []byte
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		masked, _ = hex.DecodeString(scanner.Text())
+		res, _ = singleCharacterXorFinder(masked)
+		score = scoreText(string(res))
+		if score > maxScore {
+			maxScore = score
+			unmasked = res
+		}
+	}
+	t.Logf("Message: %s", string(unmasked))
 }
